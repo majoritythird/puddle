618eef1bccc85c11a5f123669c2046e918d1cc6d
Wes Gibbs <wesgibbs@gmail.com>
2014-03-29 19:12:19 -0700

    checkpoint

--- Puddle/MyScene.m
+++ Puddle/MyScene.m
@@ -6,6 +6,7 @@
 //  Copyright (c) 2014 Wes Gibbs. All rights reserved.
 //
 
+#import <CoreMotion/CoreMotion.h>
 #import "MyScene.h"
 
 static const uint32_t wallCategory     =  0x1 << 0;
@@ -14,7 +15,10 @@ static const uint32_t critterCategory  =  0x1 << 1;
 @interface MyScene ()
 <SKPhysicsContactDelegate>
 
+@property(nonatomic,strong) NSDate *lastContactSoundPlayedAt;
+@property(nonatomic,strong) CMMotionManager *motionManager;
 @property(nonatomic,strong) SKSpriteNode *mySprite;
+@property(nonatomic,strong) CMAttitude *referenceAttitude;
 
 @end
 
@@ -44,18 +48,38 @@ static const uint32_t critterCategory  =  0x1 << 1;
     _mySprite.physicsBody = [SKPhysicsBody bodyWithRectangleOfSize:_mySprite.size];
     _mySprite.physicsBody.categoryBitMask = critterCategory;
     _mySprite.physicsBody.contactTestBitMask = critterCategory;
-    _mySprite.physicsBody.friction = 0.0;
-    _mySprite.physicsBody.linearDamping = 0.0;
-    _mySprite.physicsBody.restitution = 1;
+    _mySprite.physicsBody.friction = 0.2;
+    _mySprite.physicsBody.linearDamping = 0.2;
+    _mySprite.physicsBody.restitution = 0.8;
     [self addChild:_mySprite];
-    [_mySprite.physicsBody applyImpulse:CGVectorMake(5.0f, -0.5f)];
+    [self runAction:[SKAction playSoundFileNamed:@"appear.mp3" waitForCompletion:NO]];
+//    [_mySprite.physicsBody applyImpulse:CGVectorMake(5.0f, -0.5f)];
 
     SKPhysicsBody *wallBody = [SKPhysicsBody bodyWithEdgeLoopFromRect:self.frame];
     self.physicsBody = wallBody;
     self.physicsBody.categoryBitMask = wallCategory;
-    self.physicsBody.friction = 0.0;
-    self.physicsBody.linearDamping = 0.0;
-    self.physicsBody.restitution = 1;
+    self.physicsBody.friction = 0.2;
+    self.physicsBody.restitution = 0.2;
+
+    _motionManager = [[CMMotionManager alloc] init];
+    if([_motionManager isDeviceMotionAvailable]) {
+      [_motionManager setAccelerometerUpdateInterval:1.0/30.0];
+      [_motionManager startDeviceMotionUpdatesToQueue:[NSOperationQueue new] withHandler:^(CMDeviceMotion *motion, NSError *error)
+       {
+         if(!_referenceAttitude) {
+           _referenceAttitude = motion.attitude;
+         }
+         if(!self.scene.isPaused) {
+           CMAttitude *attitude = motion.attitude;
+           // Multiply by the inverse of the reference attitude so motion is relative to the start attitude.
+           [attitude multiplyByInverseOfAttitude:_referenceAttitude];
+           for (SKSpriteNode *child in self.scene.children) {
+             [child.physicsBody applyImpulse:CGVectorMake(attitude.roll * 0.2, -attitude.pitch * 0.2)];
+           }
+           //_mySprite.physicsBody applyImpulse:CGVectorMake(attitude.roll * 0.6, -attitude.pitch * 0.6)];
+         }
+       }];
+    }
 
     self.physicsWorld.gravity = CGVectorMake(0.0,0.0);
     self.physicsWorld.contactDelegate = self;
@@ -67,6 +91,7 @@ static const uint32_t critterCategory  =  0x1 << 1;
 
 - (void)addSpriteNamed:(NSString *)name withImageNamed:(NSString *)imageName
 {
+  [self runAction:[SKAction playSoundFileNamed:@"appear.mp3" waitForCompletion:NO]];
   CGPoint location = CGPointMake(CGRectGetMidX(self.scene.frame), CGRectGetMidY(self.scene.frame));
   SKSpriteNode *sprite = [SKSpriteNode spriteNodeWithImageNamed:imageName];
   sprite.name = name;
@@ -75,16 +100,23 @@ static const uint32_t critterCategory  =  0x1 << 1;
   [sprite runAction:[SKAction repeatActionForever:action]];
   sprite.physicsBody = [SKPhysicsBody bodyWithRectangleOfSize:sprite.size];
   sprite.physicsBody.categoryBitMask = critterCategory;
-  sprite.physicsBody.friction = 0.0;
-  sprite.physicsBody.linearDamping = 0.0;
-  sprite.physicsBody.restitution = 1;
+  sprite.physicsBody.friction = 0.2;
+  sprite.physicsBody.linearDamping = 0.2;
+  sprite.physicsBody.restitution = 0.8;
   [self addChild:sprite];
-  [sprite.physicsBody applyImpulse:CGVectorMake(25, 5)];
+//  [sprite.physicsBody applyImpulse:CGVectorMake(25, 5)];
 }
 
 - (void)critter:(SKSpriteNode *)critter1 didCollideWithCritter:(SKSpriteNode *)critter2
 {
-  [self runAction:[SKAction playSoundFileNamed:@"contact.mp3" waitForCompletion:NO]];
+  if (self.lastContactSoundPlayedAt == nil) {
+    [self runAction:[SKAction playSoundFileNamed:@"contact.mp3" waitForCompletion:NO]];
+    self.lastContactSoundPlayedAt = [NSDate date];
+  }
+  else if ([self.lastContactSoundPlayedAt timeIntervalSinceNow]*-1 > 1) {
+    [self runAction:[SKAction playSoundFileNamed:@"contact.mp3" waitForCompletion:NO]];
+    self.lastContactSoundPlayedAt = [NSDate date];
+  }
 
 //  CGSize critter1Size = critter1.size;
 //  CGSize newSize = CGSizeMake(critter1Size.width * 1.5, critter1Size.height * 1.5);
